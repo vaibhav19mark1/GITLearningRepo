d2102c863259846690ff083032dec9837111c7a0
Vaibhav <vaibhavtushar19@gmail.com>
2020-07-29 12:29:55 +0530

    updated for loop file with a message

--- for_loop.cpp
+++ for_loop.cpp
@@ -3,5 +3,7 @@ using namespace std;
 int main(){
     for(int i=0;i<5;i++)
     cout<<"Yaaaayaayaayay"<<endl;
+    cout<<"Done with for loops";
+    cout<<"what about while loops";
     return 0;
 }
